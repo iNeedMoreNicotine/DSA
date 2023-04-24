9390ba0069245d621b5f89127606b8792d185738
Alex Chen <alexpychen@gmail.com>
2023-04-24 17:52:59 +0800

    new

--- 8_Sorts.c
+++ 8_Sorts.c
@@ -46,8 +46,61 @@ void insertionSort(int arr[], int len){
     }
 }
 
+// Heap
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// implement heap data structute in an array...
+// n: length of array
+// i: index of current heap node
+void maxHeapify(int arr[], int n, int i){
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] > arr[largest]){
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]){
+        largest = right;
+    }
+
+    if (largest != i){
+        swap(&arr[i], &arr[largest]);
+        maxHeapify(arr, n, largest);
+    }
+}
+
+void buildMaxHeap(int arr[], int n){
+    for (int i = n/2 - 1; i >= 0; i--){
+        maxHeapify(arr, n, i);
+    }
+}
+
+void heapSort(int arr[], int n){
+    buildMaxHeap(arr, n);
+
+    for(int i = n-1; i >= 1; i--){
+        swap(&arr[0], &arr[i]);
+        maxHeapify(arr, i, 0);
+    }
+}
+
+void bubbleSort(int arr[], int len){
+    for(int i = 0; i < len; i++){
+        for(int j = 0; j < len - 1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
 void initArr(int arr[], int len){
-    srand(69);
+    srand(526969);
     for(int i = 0; i < len; i++){
         arr[i] = randint(0, 69);
     }
@@ -92,8 +145,20 @@ int main(){
     printf("\n");
 
     // heap sort
-    // tree sort
+    initArr(arr, length);
+    printf("Heap sort:\n");
+    heapSort(arr, length);
+    printArr(arr, length);
+    printf("\n");
+    
     // bubble sort
+    initArr(arr, length);
+    printf("Bubble sort:\n");
+    bubbleSort(arr, length);
+    printArr(arr, length);
+    printf("\n");
+
     // quick sort
+    
     // merge sort
 }
